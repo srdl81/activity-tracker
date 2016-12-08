789bd478313bd22ae04f3e83884fe66155569d92
xelikan <xelikan007@gmail.com>
2016-12-08 17:51:29 +0100

    can handle a list of jobids

--- src/main/java/ams/labs/Application.java
+++ src/main/java/ams/labs/Application.java
@@ -2,8 +2,10 @@ package ams.labs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableNeo4jRepositories("ams.labs.repository")
@@ -13,5 +15,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
--- src/main/java/ams/labs/controller/LogActivityController.java
+++ src/main/java/ams/labs/controller/LogActivityController.java
@@ -9,27 +9,64 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
 import javax.validation.Valid;
+import java.util.Arrays;
 
 @Api(value = "Log Activity", description = "Track User Activites")
 @RestController
 public class LogActivityController {
 
     private final static Logger log = LoggerFactory.getLogger(LogActivityController.class);
+    private static final String URL = "http://pilot.arbetsformedlingen.se:80/pbv3api/rest/matchning/v1/matchandeRekryteringsbehov/";
 
     @Autowired
     private ActivityService activityService;
 
+    @Autowired
+    private RestTemplate restTemplate;
 
     @ApiOperation(value = "Log User Activity", nickname = "Log Activity", produces = "application/json")
     @RequestMapping(value = "/user/{userId}/activity", method = RequestMethod.POST)
     public JobAdvertisement logUserActivity(@Valid @RequestBody MatchResultDTO matchResultDTO,
                                             @PathVariable("userId") Long userId) {
 
-
         log.info(String.format("logging activity for DTO '%s'", matchResultDTO));
+        return logActivity(userId, matchResultDTO);
+    }
+
+    @ApiOperation(value = "Generate activity", nickname = "Log Activity", produces = "application/json")
+    @RequestMapping(value = "/user/{userId}/job/{jobIds}", method = RequestMethod.POST)
+    public String logUserActivity(@PathVariable("userId") Long userId, @PathVariable("jobIds") String jobIds) {
+
+        for (String id : Arrays.asList(jobIds.split(","))) {
+            ResponseEntity<MatchResultDTO> responseEntity = restTemplate.postForEntity(URL + id, getMultiValueMapHttpEntity(), MatchResultDTO.class);
+
+            if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                activityService.logActivity(responseEntity.getBody(), userId);
+            }
+        }
+
+        return "SUCCESS";
+    }
+
+
+
+    private JobAdvertisement logActivity(@PathVariable("userId") Long userId, MatchResultDTO matchResultDTO) {
         return activityService.logActivity(matchResultDTO, userId);
+    }
+
+    private HttpEntity<MultiValueMap<String, String>> getMultiValueMapHttpEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        return new HttpEntity<>(map, headers);
     }
 
 }
