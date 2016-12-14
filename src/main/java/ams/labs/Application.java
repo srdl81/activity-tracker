9c363d1982c4c2881d3cffea50a1de5defb8e617
xelikan <xelikan007@gmail.com>
2016-12-14 21:11:48 +0100

    neo4j conf added in application class

--- src/main/java/ams/labs/Application.java
+++ src/main/java/ams/labs/Application.java
@@ -1,20 +1,42 @@
 package ams.labs;
 
+import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableNeo4jRepositories("ams.labs.repository")
 @EnableTransactionManagement
-public class Application {
+@EnableNeo4jRepositories("ams.labs.repository")
+@EnableAutoConfiguration
+@Configuration
+public class Application extends Neo4jConfiguration {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    public static final String URL = System.getenv("NEO4J_URL") != null ? System.getenv("NEO4J_URL") : "http://neo4j:elvis@localhost:7474";
+
+    @Bean
+    public org.neo4j.ogm.config.Configuration getConfiguration() {
+        org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
+        config.driverConfiguration()
+                .setDriverClassName("org.neo4j.ogm.drivers.http.driver.HttpDriver")
+                .setURI(URL);
+        return config;
+    }
+
+    @Override
+    public SessionFactory getSessionFactory() {
+        return new SessionFactory(getConfiguration(), "ams.labs.entity");
+    }
 
     @Bean
     public RestTemplate restTemplate() {
