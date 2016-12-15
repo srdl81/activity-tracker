b196e11da402886906b529808379bf534181669c
xelikan <xelikan007@gmail.com>
2016-12-15 15:37:38 +0100

    added test for profession repo

---
+++ src/test/java/ams/labs/repository/ProfessionRepositoryTest.java
@@ -0,0 +1,40 @@
+package ams.labs.repository;
+
+import ams.labs.configuration.Neo4jTestConfiguration;
+import ams.labs.entity.Profession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Neo4jTestConfiguration.class)
+@ActiveProfiles(profiles = "test")
+public class ProfessionRepositoryTest {
+
+    private static final String SYSTEMUTVECKLARE = "Systemutvecklare";
+    private static final Long PROFESSION_ID = new Long(2255);
+    @Autowired
+    private ProfessionRepository repository;
+
+    @Test
+    public void findByProfessionId() throws Exception {
+        //Given:
+        Profession profession = new Profession();
+        profession.setName(SYSTEMUTVECKLARE);
+        profession.setProfessionId(PROFESSION_ID);
+        repository.save(profession);
+
+        //When:
+        Profession result = repository.findByProfessionId(PROFESSION_ID);
+
+        //Then:
+        assertNotNull(result);
+        assertEquals(result, result);
+    }
+
+}
